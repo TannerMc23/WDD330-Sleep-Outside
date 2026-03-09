55d88e972c2dae83e6a56847eb2ea239f88324b1
miwe-max <miwe@byupathway.edu>
2026-03-09 18:48:09 +0100

    npm run lint/format code

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,8 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const productList = getLocalStorage("so-cart") || []
-  productList.push(product)
+  const productList = getLocalStorage("so-cart") || [];
+  productList.push(product);
   setLocalStorage("so-cart", productList);
 }
 // add to cart button event handler
