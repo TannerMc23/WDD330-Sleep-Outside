8b7539a48687d182a968955e3e28ec8de2a47acb
miwe-max <miwe@byupathway.edu>
2026-03-09 18:05:18 +0100

    Add Item to Cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,12 +4,10 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  const productList = getLocalStorage("so-cart") || []
+  productList.push(product)
+  setLocalStorage("so-cart", productList);
 }
-
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
