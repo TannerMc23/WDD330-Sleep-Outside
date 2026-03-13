645fed67a34e5cf32ef0ba3ad2d36d27d7ec57dc
Jeanpierre Muamba <admin@Admins-MacBook-Air.local>
2026-03-13 23:05:13 +0100

    new commit for activity week02

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,12 @@
-import { getLocalStorage,getParam } from "./utils.mjs";
+import {getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import productDetails  from "./productDetails";
+
+
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
+const product = new productDetails(productId, dataSource);
+product.init();
 
-
-
-//TEAM ACTIVITY 2
-
-//Call the getParam function to see if we have the url
-
-const productId= getParam('product');
-
-console.log('=================');
-
-console.log('Le resultat est');
-console.log('-----------------');
-console.log(dataSource.findProductById(productId));
-console.log('=================');
-
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
--- src/js/productDetails.js
+++ src/js/productDetails.js
@@ -1,25 +1,52 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 
 export default class productDetails{
     constructor(productId, datasource){
-        this.productId=productId;
-        this.product=[];
-        this.datasource=datasource;
+        this.productId = productId;
+        this.product = [];
+        this.datasource = datasource;
+        this.productList=[];
 
     }
 
-         renderProductDetails(){
-        //gere we put details of the card
-    }
-    Infinity(){
-        //initialise
-    }
+    
+    async init(){
 
-     addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
+        this.product = await this.datasource.findProductById(this.productId);
+        this.renderProductDetails();
+        // add listener to Add to Cart button
+    document
+       .getElementById("addToCart")
+       .addEventListener("click",this.addProductToCart.bind(this));
+    }
+                 renderProductDetails() {
+            productTemplate(this.product)
+          }
 
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+           addProductToCart(product) {
+                const cartItems = this.product;
+                this.productList.push(cartItems);
+
+                console.log(this.productList);
+           }
+            
 }
-}
+
+ function productTemplate(product){
+        const parent = document.querySelector(".product-detail");
+
+        const details = `
+                <h3>${product.Name}</h3>
+                <h2>${product.NameWithoutBrand}</h2>
+                <img  class="divider" src=${product.Image} alter="${product.NameWithoutBrand}">
+                <p class="product-card__price"> $ ${product.FinalPrice}</p>
+                <p class="product__color">${product.Colors[0].ColorName}</p>
+
+                 <div class="product-detail__add">
+                        <button id="addToCart" data-id="${product.Id}">Add to Cart</button>
+                </div>
+            `;
+
+        parent.innerHTML=details;
+    }
