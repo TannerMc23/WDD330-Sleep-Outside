946abbe90792ecfff842efb0c035149f3bd4bba6
tannermc23 <tmcmillan@byupathway.edu>
2026-03-25 20:03:21 -0600

    Completed last couple steps

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -6,13 +6,13 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   constructor(category) {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   } 
   
-  getData() {
+  async getData() {
     return fetch(this.path)
       .then(convertToJson)
       .then((data) => data);
@@ -20,5 +20,15 @@ export default class ProductData {
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
+  }
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(`${baseURL}checkout/`, options).then(convertToJson);
   }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,12 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import ExternalServices from "./ExternalServices.mjs";
 import ProductList from "./ProductList.mjs";
 import Alert from './Alert.js';
 
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("tents");;
+const dataSource = new ExternalServices("tents");;
 const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 
